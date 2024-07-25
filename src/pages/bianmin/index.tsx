809c4850c2fef6b9318e5d7cca42072c26e10f96
Al Zee <z@alz.ee>
2024-07-26 04:05:30 +0800

    nth

--- src/pages/bianmin/index.tsx
+++ src/pages/bianmin/index.tsx
@@ -6,15 +6,15 @@ import Card from '../../components/card'
 
 function Index() {
   return (
-    <WebView src='https://h5.clewm.net/?url=http%3A%2F%2Fh.qr61.cn%2FocFiYI%2FqFnQwwE' />
+    // <WebView src='https://h5.clewm.net/?url=http%3A%2F%2Fh.qr61.cn%2FocFiYI%2FqFnQwwE' />
 
-    // <View className="p-1">
-    //   <View className="page-title mb-2">便民服务</View>
-    //   <a href="http://h.qr61.cn/ocFiYI/q1OtanR?category_id%3D41187033%26org_module_id%3D38267485%26type%3D0%26list_name%3D部门通%26user_id%3D16949321">
-    //     <Card title="府院联动" desc="沟通法院与主要业务往来职能部门，将线下操作转变为线上办理。为当事人提供便捷的诉讼服务。" color="#ffffff" bgcolor="#20985a" />
-    //   </a>
-    //   <Card title="鄂汇办" desc="通过业务协同和数据共享，优化办事体验，实现群众办事“指尖办理”。" color="#ffffff" bgcolor="#20985a" />
-    // </View>
+    <View className="p-1">
+      <View className="page-title mb-2">便民服务</View>
+      <a href="http://h.qr61.cn/ocFiYI/q1OtanR?category_id%3D41187033%26org_module_id%3D38267485%26type%3D0%26list_name%3D部门通%26user_id%3D16949321">
+        <Card title="府院联动" desc="沟通法院与主要业务往来职能部门，将线下操作转变为线上办理。为当事人提供便捷的诉讼服务。" />
+      </a>
+      <Card title="鄂汇办" desc="通过业务协同和数据共享，优化办事体验，实现群众办事“指尖办理”。" />
+    </View>
   )
 }
 
--- src/pages/feedback/index.tsx
+++ src/pages/feedback/index.tsx
@@ -1,12 +1,39 @@
 import React from 'react'
-import { View } from '@tarojs/components'
-import { Button } from "@nutui/nutui-react-taro"
+import { View, Button, Radio, RadioGroup, Textarea, Input } from '@tarojs/components'
+import { TextArea } from '@nutui/nutui-react-taro';
+import { User, Phone } from '@nutui/icons-react-taro'
 import './index.scss'
 
 function Index() {
   return (
     <View className="p-1">
       <View className="page-title mb-2">在线留言</View>
+      <View className="desc">
+      您好，为给您提供更加优质、高效的诉讼服务，欢迎您在线咨询留言。工作人员会对您的个人信息保密，请放心填写，谢谢!
+      </View>
+
+      <View className="section-title"> 您要反馈哪方面的问题？ </View>
+      <View className="">
+      <RadioGroup>
+        <Radio value="0" checked='false'>诉讼</Radio>
+        <Radio value="1" checked='false'>执行</Radio>
+      </RadioGroup>
+      </View>
+
+      <View className="section-title"> 详细描述？ </View>
+      <View className="note"> 请详细说明目前的问题。 </View>
+      <TextArea className="input" rows="5" />
+
+      <View className="section-title"> 反馈人 </View>
+      <Input className="input" type='text' />
+
+      <View className="section-title"> 身份证号</View>
+      <Input className="input" type='text' />
+
+      <View className="section-title"> 手机号</View>
+      <Input className="input" type='text' />
+
+      <Button type="warn">提交</Button>
     </View>
   )
 }
